df0a1b68baacd4d992e1ceba05e8fa4457776263
Kanaan Wray <kanaanwray@yahoo.com>
2026-05-04 15:04:45 -0500

    backend state config

--- Full-AzureLab-Template/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "resource_group_name" {}
-variable "location" {}
-
-variable "vnet_name" {}
-variable "vnet_address_space" {}
-
-variable "subnet_name" {}
-variable "subnet_address_space" {}
